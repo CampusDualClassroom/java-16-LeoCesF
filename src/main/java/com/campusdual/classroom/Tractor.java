fe7aa40df28aaefd12413bb611dc4542dc8ade2d
LeoCesF <leo.cesfra.goso@gmail.com>
2025-05-26 09:28:51 +0200

    Ejercicio 16 - 26/05/2025 - 9:28

--- src/main/java/com/campusdual/classroom/Tractor.java
+++ src/main/java/com/campusdual/classroom/Tractor.java
@@ -1,11 +1,25 @@
 package com.campusdual.classroom;
 
-public class Tractor{
+public class Tractor implements IMachine {
 
 	protected int horsePower = 0;
 	
 	public Tractor(int hp) {
 		this.horsePower = hp;
+	}
+
+	public void start() {
+		System.out.println("Tractor está encendido");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Tractor está apagado");
+	}
+
+	@Override
+	public void maintenance() {
+		System.out.println("Tractor se encuentra realizando un mantenimiento");
 	}
 
 	public void forward() {
